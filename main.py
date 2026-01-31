2b7c6efa24e971665d247eaed74176f890d6cca4
Starship100 <jimmy.widlund@outlook.com>
2026-01-31 18:44:08 +0100

    cloned

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-temp = float(input("Ange en temperatur i grader celsius: "))
+
+
+# Version 1
+temp = float(input("Ange en temperatur i grader Celsius: "))
 farenheit = temp * 9 / 5 + 32
-print("Det blir ", farenheit , "grader i farenheit.")
+print("Det blir ", farenheit , "grader i Farenheit.")
+
+# Version 2
+val = input("Ange temperatur i Fahranheit(f) eller Celsius(c): ")
+if val == "c":
+    celsius = float(input("Skriv in temperatur i grader Celsius: "))
+    #fahrenheit = 1.8 * celsius + 32
+    fahrenheit = celsius * 9 / 5 + 32
+    print("Det blir ", farenheit , "grader Fahrenheit.")
+
+"""
+val = input("Vill du ange temperaturen i Celsius eller Fahrenheit? (C/F): ")
+
+if val == "C":
+    celsius = float(input("Skriv in temperaturen i grader Celsius: "))
+    fahrenheit = 1.8 * celsius + 32
+    print(f"Det blir {fahrenheit} grader Fahrenheit.")
+    
+"""
