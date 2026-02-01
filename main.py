361b64454075a15c3ae94ab37ab108860b03e301
Starship100 <jimmy.widlund@outlook.com>
2026-02-01 02:33:15 +0100

    if-sats

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
-temp = float(input("Ange en temperatur i grader celsius: "))
-farenheit = temp * 9 / 5 + 32
-print("Det blir ", farenheit , "grader i farenheit.")
 
 # Version 1
-temp = float(input("Ange en temperatur i grader Celsius: "))
-farenheit = temp * 9 / 5 + 32
-print("Det blir ", farenheit, "grader i Farenheit.")
+#celsius = float(input("Ange en temperatur i grader celsius: "))
+#farenheit = 1.8 * celsius + 32
+#print("Det blir ", farenheit , "grader i farenheit.")
+
 
 # Version 2
 val = input("Ange temperatur i Fahranheit(f) eller Celsius(c): ")
 if val == "c":
     celsius = float(input("Skriv in temperatur i grader Celsius: "))
-    # fahrenheit = 1.8 * celsius + 32
-    fahrenheit = celsius * 9 / 5 + 32
-    print("Det blir ", farenheit, "grader Fahrenheit.")
-
-"""
-val = input("Vill du ange temperaturen i Celsius eller Fahrenheit? (C/F): ")
-
-if val == "C":
-    celsius = float(input("Skriv in temperaturen i grader Celsius: "))
     fahrenheit = 1.8 * celsius + 32
-    print(f"Det blir {fahrenheit} grader Fahrenheit.")
+    print("Det blir ", fahrenheit, "grader Fahrenheit.")
+elif val == "f":
+    fahrenheit = float(input("Skriv in temperatur i grader Fahrenheit: "))
+    celsius = (fahrenheit - 32) / 1.8
+    print("Det blir ", celsius, "grader Celsius.")
 
-"""
+    if celsius < 10:
+        print("Ta på dig vinterkläder!")
+    elif celsius > 20:
+        print("Packa badkläder!")
